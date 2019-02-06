bdd2f0ea3ca39aefa7f27f298585b3bf41560712
Mark Cohen <mark.cohen@mcla.edu>
2019-02-06 19:39:57 +0000

    fixed bug in find_highest.py

--- python_examples/find_highest.py
+++ python_examples/find_highest.py
@@ -6,11 +6,11 @@ def highest(filename):
         line = line[:-1]
         tokens = line.split(',')
         for i in range(1, len(tokens)):
+            if tokens[0] not in result:
+                result[tokens[0]] = 0;
             tokens[i] = int(tokens[i])
-        if tokens[0] not in result:
-            result[tokens[0]] = 0;
-        if max(tokens[1:]) > result[tokens[0]]:
-            result[tokens[0]] = max(tokens[1:])
+            if tokens[i] > result[tokens[0]]:
+                result[tokens[0]] = tokens[i]
     print(result)
         
 highest('test.txt')
