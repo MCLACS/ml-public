c01ca72fdfa3005ffa85b7a0904b921eb55dc968
Mark Cohen <mark.cohen@mcla.edu>
2021-01-05 08:36:17 -0500

    added numpy tutorial and changes to cricket

---
+++ 01-NumPyTutorial/tutorial.py
@@ -0,0 +1,70 @@
+# import the numpy library...
+import numpy as np
+
+# create a list of list using the built-in
+# python list data structure...
+listOfLists = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+print("List of lists:\n%s\n" % listOfLists)
+
+# convert the list of lists to a numpy matrix...
+matrix = np.array(listOfLists)
+print("NumPy matrix:\n%s\n" % matrix)
+
+# turn it back into a list...
+listAgain = matrix.tolist()
+print("List Again:\n%s\n" % listAgain)
+
+# methods that describe the matrix structure
+a = np.array([ [1, 2, 3], [4, 5, 6], [7, 8, 9] ])
+b = np.array([ [1, 1, 1], [1, 1, 1], [2, 2, 2] ])
+
+print("a = \n%s\n" % a)
+
+print("Type: %s" % type(matrix))
+print("Shape: %s" % str(matrix.shape))
+print("Size: %d" % matrix.size)
+print("Sum of all elements: %s" % a.sum())
+print("Min of all elements: %s" % a.min())
+print("Max of all elements: %s" % a.max())
+print("Column sums of a: %s" % a.sum(axis=0))
+print("Row sums of a: %s\n" % a.sum(axis=1))
+
+# build a large matrix with default values...
+matrix = np.arange(10000).reshape(100,100)
+print("NumPy large matrix:\n%s\n" % matrix)
+
+# matrix arithmetic
+a = np.array([ [1, 2, 3], [4, 5, 6], [7, 8, 9] ])
+b = np.array([ [1, 1, 1], [1, 1, 1], [2, 2, 2] ])
+
+print("a = \n%s\n" % a)
+print("b = \n%s\n" % b)
+
+c = a - b
+print("a - b = \n%s\n" % c)
+
+c = a*2
+print("a*2 = \n%s\n" % c)
+
+c = a**2
+print("a**2 = \n%s\n" % c)
+
+c = a/2
+print("a/2 = \n%s\n" % c)
+
+c = a>2
+print("a>2 = \n%s\n" % c)
+
+c = a*b
+print("a*b = \n%s\n" % c)
+
+# indexing
+print("a = \n%s\n" % a)
+print("a[2,0] = %s\n" % a[2,0])
+
+# slicing
+print("a = \n%s\n" % a)
+
+print("a[0:5, 1] = %s\n" % a[0:5, 1])
+print("a[2,0:3] = %s\n" % a[2,0:3])
+print("a[0:2,0:3] = \n%s\n" % a[0:2,0:3])
--- 02-LinearRegression/crickets-python/train-test-predict.py
+++ 02-LinearRegression/crickets-python/train-test-predict.py
@@ -12,12 +12,21 @@ X = f.listTo2DNumPy(x)
 y = np.loadtxt('CRICK-Y.csv',skiprows=1, unpack=False, delimiter=',')
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
 
-print('\nTraining...')
+print('Training set shape= %s' % str(X_train.shape))
+print('Test set shape= %s' % str(X_test.shape))
+
+print('\nExcel results...')
+lr = LinearRegression().fit(X_train , y_train)
+print("Excel Coef: %f" % 0.8918)
+print("Excel Intercept: %f" % 40.025)
+print( "Excel score linear: %.2f" % 0.9609)
+
+print('\nTraining results...')
 lr = LinearRegression().fit(X_train , y_train)
 print("lr.coef_: %f" % lr.coef_)
 print("lr.intercept_: %f" % lr.intercept_)
 
-print('\nTesting...')
+print('\nTesting results...')
 print( "Training set score linear: %.2f" % lr.score( X_train , y_train))
 print( "Test set score linear: %.2f" % lr.score(X_test , y_test))
 
@@ -26,3 +35,14 @@ freq = float(input("Enter a chirp frequency:"))
 X_new_sample = np.array([[freq]])
 y_classification = lr.predict(X_new_sample)
 print('Prediction: %s->%s' % (X_new_sample[0], y_classification[0]))
+
+plt.scatter(x, y)
+plt.xlabel('Chirp Freq')
+plt.ylabel('Temp')
+plt.title('Crickets Chirp vs. Temp')
+plt.xlim(left=0)
+plt.ylim(bottom=0)
+x_values = [x.min(), x.max()]
+y_values = [lr.intercept_ + lr.coef_*x.min(), lr.intercept_ + lr.coef_*x.max()]
+plt.plot(x_values, y_values , marker = 'o', color='red')
+plt.show()
