18a58249ab37a149ef0d70ee6daca0392103a221
Mark Cohen <mark.cohen@mcla.edu>
2021-01-06 08:41:16 -0500

    added you try it assignment

--- Old/scale/scale_example.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler 
-
-X = np.array([[ 1., -1.,  2.], [ 2.,  0.,  0.], [ 0.,  1., -1.]])
-scaler = MinMaxScaler() 
-X_scaled = scaler.fit_transform(X) 
-print(X_scaled)
-
